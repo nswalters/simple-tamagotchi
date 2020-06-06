2a03bc98d299537547ec7d41cf63cba81754ceb6
Nick Walters <nswalters.s@gmail.com>
2020-06-06 09:20:41 -0500

    Fix functions

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -5,10 +5,19 @@ import '../styles/play.scss';
 import '../styles/fight.scss';
 import '../styles/sleep.scss';
 import eat from './components/eat';
+import play from './components/play';
+import fight from './components/fight';
+import sleep from './components/sleep';
 
 const init = () => {
   eat.buildBaseDomString();
   eat.addButtonListeners();
+  play.buildBaseDomString();
+  play.addButtonListeners();
+  fight.buildBaseDomString();
+  fight.addButtonListeners();
+  sleep.buildBaseDomString();
+  sleep.addButtonListeners();
 };
 
 init();
